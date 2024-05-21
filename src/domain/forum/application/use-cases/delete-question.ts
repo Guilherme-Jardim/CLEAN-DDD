eb03fafc41a65975f8790ecdfaaedbc4b3fe3846
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-05-21 09:13:44 -0300

    use-cases&tests update

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -1,11 +1,17 @@
+import { Either, right } from '@/core/either'
 import { QuestionsRepository } from '../repositories/questions-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface DeleteQuestionUseCaseRequest {
   authorId: string
   questionId: string
 }
 
-interface DeleteQuestionUseCaseResponse {}
+type DeleteQuestionUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
@@ -25,6 +31,6 @@ export class DeleteQuestionUseCase {
 
     await this.questionsRepository.delete(question)
 
-    return {}
+    return right({})
   }
 }
